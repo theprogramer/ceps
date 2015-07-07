adf5589067679dbed744dd0e80186472e43e838a
Thiago Miranda <theprogramer@theprogramer.com.br>
2015-07-07 11:25:34 -0300

    Update setup

--- lib/ceps/setup.rb
+++ lib/ceps/setup.rb
@@ -1,37 +1,41 @@
 module Correios
 
+
   ##
   # Handles building the in memory store of CEP data
   class Setup
 
-    attr_accessor :data_file_name
+    attr_writer :data_file_name
 
-    attr_accessor :data_file_path
+    attr_writer :data_file_path
 
-    attr_accessor :data_file
+    attr_writer :data_file
 
     def ceps
-      return @ceps if instance_variable_defined?('@ceps')
+      return @ceps if instance_variable_defined?("@ceps")
       @ceps ||= load
     end
 
-    def initialize
-      @data_file_name = 'ceps.yaml'
-      if defined?(Rails)
-        @data_file_path = File.join(Rails.root, "lib", "data")
-      else
-        @data_file_path = File.join( 
-                            File.dirname(
-                            File.dirname(__FILE__)),
-                            "lib", "data")
-      end
-      @data_file = File.join @data_file_path, @data_file_name
+    def data_file_name
+      @data_file_name || "ceps.yaml"
+    end
+
+    def data_file_path
+      @data_file_path || default_file_path
+    end
+
+    def data_file
+      @data_file || File.join(data_file_path, data_file_name)
     end
 
     private
 
       def load
         YAML.load_file(@data_file)
+      end
+
+      def default_file_path
+        File.join(File.dirname(File.dirname(__FILE__)), "data")
       end
 
   end
