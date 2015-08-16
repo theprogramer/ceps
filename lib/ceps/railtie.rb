4a5cffc269e35eb3560bc5aa08bd8a1176784d05
Thiago Miranda <theprogramer@theprogramer.com.br>
2015-08-16 01:19:12 -0300

    Bugfix

--- lib/ceps/railtie.rb
+++ lib/ceps/railtie.rb
@@ -7,9 +7,7 @@ module Ceps
     railtie_name :ceps
 
     rake_tasks do
-      rake_tasks do
-        Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
-      end
+      Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
     end
 
   end
